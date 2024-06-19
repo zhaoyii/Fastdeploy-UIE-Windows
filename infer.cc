b17d27390be65c1a61c5323b069e98e320162e0b
yunus <532708564@qq.com>
2024-06-19 17:50:27 +0800

    添加处理输入 utf-8 字符串函数

--- infer.cc
+++ infer.cc
@@ -21,6 +21,7 @@
 #include <codecvt>
 
 #include "fastdeploy/text.h"
+#include <windows.h>
 
 using namespace paddlenlp;
 
@@ -30,22 +31,50 @@ const char sep = '\\';
 const char sep = '/';
 #endif
 
+std::wstring arg_to_wstring(char *arg)
+{
+  int wide_size = MultiByteToWideChar(CP_ACP, 0, arg, -1, NULL, 0);
+  wchar_t *wide_str = new wchar_t[wide_size];
+  MultiByteToWideChar(CP_ACP, 0, arg, -1, wide_str, wide_size);
+  std::wstring wtext(wide_str);
+  delete[] wide_str;
+
+  return wtext;
+}
+
+std::string wide_to_utf8(const std::wstring &wstr)
+{
+  if (wstr.empty())
+    return std::string();
+  int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
+  std::string strTo(size_needed, 0);
+  WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
+  return strTo;
+}
+
+std::string arg_to_utf8(char *arg)
+{
+  std::wstring wtext = arg_to_wstring(arg);
+  std::string text = wide_to_utf8(wtext);
+  return text;
+}
+
 int main(int argc, char *argv[])
 {
 
-  if (argc != 3 && argc != 4)
-  {
-    std::cout << "Usage: infer_demo /path/to/model device [backend], "
-                 "e.g ./infer_demo uie-base 0 [0]"
-              << std::endl;
-    std::cout << "The data type of device is int, 0: run with cpu; 1: run "
-                 "with gpu."
-              << std::endl;
-    std::cout << "The data type of backend is int, 0: use paddle backend; 1: "
-                 "use onnxruntime backend; 2: use openvino backend. Default 0."
-              << std::endl;
-    return -1;
-  }
+  // if (argc != 3 && argc != 4)
+  // {
+  //   std::cout << "Usage: infer_demo /path/to/model device [backend], "
+  //                "e.g ./infer_demo uie-base 0 [0]"
+  //             << std::endl;
+  //   std::cout << "The data type of device is int, 0: run with cpu; 1: run "
+  //                "with gpu."
+  //             << std::endl;
+  //   std::cout << "The data type of backend is int, 0: use paddle backend; 1: "
+  //                "use onnxruntime backend; 2: use openvino backend. Default 0."
+  //             << std::endl;
+  //   return -1;
+  // }
   auto option = fastdeploy::RuntimeOption();
   if (std::atoi(argv[2]) == 0)
   {
@@ -87,6 +116,8 @@ int main(int argc, char *argv[])
   using fastdeploy::text::SchemaNode;
   using fastdeploy::text::UIEResult;
 
+  auto utf8_str = arg_to_utf8(argv[4]);
+
   try
   {
     auto predictor = fastdeploy::text::UIEModel(
@@ -95,9 +126,7 @@ int main(int argc, char *argv[])
     std::cout << "After init predictor" << std::endl;
     std::vector<std::unordered_map<std::string, std::vector<UIEResult>>> results;
     // Named Entity Recognition
-    predictor.Predict({"2月8日上午北京冬奥会自由式滑雪女子大跳台决赛中中国选手谷"
-                       "爱凌以188.25分获得金牌！"},
-                      &results);
+    predictor.Predict({utf8_str}, &results);
 
     // 设置输出中文 utf-8
     std::locale::global(std::locale("zh_CN.UTF-8"));
